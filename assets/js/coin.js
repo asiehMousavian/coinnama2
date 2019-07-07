989393e96686f18acfe09613f1ba15c2687828d5
Your Name <you@example.com>
2019-07-07 11:43:39 +0430

    16-04-98

--- assets/js/coin.js
+++ assets/js/coin.js
@@ -31,21 +31,25 @@ $(document).ready(function () {
       $(this).find('img').toggle();
     });
 
+
+   
     $(document).click(function () {
       $(".countryDropDown-wrapper").hide();
       $('.coinDropDown-wrapper').hide();
       $('.modalcountryDropDown-wrapper').hide();
     });
 
+    
+
     $('.countryInput-Wrapper').on('click', function (e) {
       e.stopPropagation();
       $('.coinDropDown-wrapper').hide();
       $('.countryDropDown-wrapper').toggle();
-
     });
 
-    $('.countryDropDown-wrapper .listItem').on('click', function () {
+  
 
+    $('.countryDropDown-wrapper .listItem').on('click', function () {
       var imgSrc = $(this).find('img').attr('src');
       var span1 = $(this).find('.lgSpan').text();
       var span2 = $(this).find('.smSpan').text();
@@ -53,6 +57,10 @@ $(document).ready(function () {
       $('.countryInput-Wrapper #lgSpan').text(span1);
       $('.countryInput-Wrapper #smSpan').text(span2);
       $('.countryDropDown-wrapper').toggle();
+    });
+
+    $('.countryDropDown-wrapper  #country-search').on('click',function(e){
+      e.stopPropagation();
     });
 
     $('.coinInput-Wrapper').on('click', function (e) {
@@ -72,7 +80,9 @@ $(document).ready(function () {
       $('.coinInput-Wrapper #smSpan').text(span2);
       $('.coinDropDown-wrapper').toggle();
     });
-
+    $('.coinDropDown-wrapper  #coin-search').on('click',function(e){
+      e.stopPropagation();
+    });
 
 //modal dropDown
 
@@ -90,5 +100,9 @@ $(document).ready(function () {
       $('.modalcountryInput-Wrapper #modallgSpan').text(span1);
       $('.modalcountryInput-Wrapper #modalsmSpan').text(span2);
       $('.modalcountryDropDown-wrapper').toggle();
+    });
+
+    $('.modalcountryDropDown-wrapper  #modal-search').on('click',function(e){
+      e.stopPropagation();
     });
 });
